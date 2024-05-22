7d446268c4b8bcdc4c349a5cedfd8a77a721085e
ayuputrii <ayuarmadani.pm@gmail.com>
2024-05-22 23:48:57 +0700

    Update cart pages

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -3,7 +3,7 @@ import {BackHeader, Buttons, CardCommons, Gap, Text} from '../../components';
 import IconANT from 'react-native-vector-icons/AntDesign';
 import {colors} from '../../utils/colors';
 import {moderateScale} from '../../utils/scale';
-import {ScrollView, View} from 'react-native';
+import {SafeAreaView, ScrollView, StatusBar, View} from 'react-native';
 import styles from './styles';
 import {CartProps} from '../../navigation';
 import {CartSections} from '../../sections';
@@ -12,7 +12,12 @@ const Cart = ({navigation}: CartProps) => {
   const [check, setCheck] = useState(false);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        animated={false}
+        backgroundColor={colors.basebg}
+        barStyle="dark-content"
+      />
       <BackHeader
         title="Transaction Cart"
         goBack={() => navigation?.goBack()}
@@ -43,6 +48,7 @@ const Cart = ({navigation}: CartProps) => {
               <Text style={styles.text1}>Rp 100.000</Text>
             </View>
             <Buttons
+              disabled={false}
               style={styles.btn}
               onPress={() => navigation.navigate('Checkout')}>
               <Text style={styles.txtCheckout}>Checkout (16)</Text>
@@ -51,7 +57,7 @@ const Cart = ({navigation}: CartProps) => {
         </CardCommons>
         <Gap height={moderateScale(8)} width={0} />
       </BackHeader>
-    </View>
+    </SafeAreaView>
   );
 };
 
